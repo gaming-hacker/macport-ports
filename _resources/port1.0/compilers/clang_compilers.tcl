5c5ca56189bc74b63b93e19b85896cbd08db5ab8
GA <gaming-hacker@users.noreply.github.com>
2022-04-23 10:41:19 +0200

    cleaning

--- _resources/port1.0/compilers/clang_compilers.tcl
+++ _resources/port1.0/compilers/clang_compilers.tcl
@@ -12,35 +12,4 @@
 
 global os.major
 
-if {${os.major} >= 11} {
-    lappend compilers macports-clang-14 \
-                      macports-clang-13 \
-                      macports-clang-12
-}
-
-if {${os.major} >= 10} {
-    lappend compilers macports-clang-11
-    if {[option build_arch] ne "arm64"} {
-        lappend compilers macports-clang-10 macports-clang-9.0
-        if {${os.major} < 20} {
-            lappend compilers macports-clang-8.0
-        }
-    }
-}
-
-if {${os.major} >= 9 && ${os.major} < 20} {
-    lappend compilers macports-clang-7.0 \
-                      macports-clang-6.0 \
-                      macports-clang-5.0
-}
-
-if {${os.major} < 16} {
-    # The Sierra SDK requires a toolchain that supports class properties
-    if {${os.major} >= 9} {
-        lappend compilers macports-clang-3.7
-    }
-    lappend compilers macports-clang-3.4
-    if {${os.major} < 9} {
-        lappend compilers macports-clang-3.3
-    }
-}
+lappend compilers macports-clang-14
